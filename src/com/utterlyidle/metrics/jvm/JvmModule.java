d9ddf8303b53f38bd9fa9ff57c6f26f5cb898237
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2013-06-12 18:15:08 +0200

    added memory usage metrics for all memory pools

--- src/com/utterlyidle/metrics/jvm/JvmModule.java
+++ src/com/utterlyidle/metrics/jvm/JvmModule.java
@@ -1,26 +1,29 @@
-package com.utterlyidle.metrics.jvm.gc;
+package com.utterlyidle.metrics.jvm;
 
 import com.codahale.metrics.Metric;
 import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.MetricSet;
 import com.codahale.metrics.jvm.GarbageCollectorMetricSet;
+import com.codahale.metrics.jvm.MemoryUsageGaugeSet;
 import com.googlecode.totallylazy.Callable2;
 import com.googlecode.totallylazy.Maps;
 import com.googlecode.utterlyidle.modules.ApplicationScopedModule;
 import com.googlecode.yadic.Container;
 
-import java.lang.management.ManagementFactory;
 import java.util.Map;
 
 public class JvmModule implements ApplicationScopedModule {
     @Override
     public Container addPerApplicationObjects(Container container) throws Exception {
-        container.addInstance(GarbageCollectorMetricSet.class, new GarbageCollectorMetricSet(ManagementFactory.getGarbageCollectorMXBeans()));
-        registerGarbageCollectorMetrics(container.get(MetricRegistry.class), container.get(GarbageCollectorMetricSet.class));
+        container.add(GarbageCollectorMetricSet.class);
+        container.add(MemoryUsageGaugeSet.class);
+        registerMetrics(container.get(MetricRegistry.class), container.get(GarbageCollectorMetricSet.class));
+        registerMetrics(container.get(MetricRegistry.class), container.get(MemoryUsageGaugeSet.class));
         return container;
     }
 
-    private void registerGarbageCollectorMetrics(MetricRegistry metricRegistry, GarbageCollectorMetricSet garbageCollectorMetricSet) {
-        Map<String, Metric> metrics = garbageCollectorMetricSet.getMetrics();
+    private void registerMetrics(MetricRegistry metricRegistry, MetricSet metricSet) {
+        Map<String, Metric> metrics = metricSet.getMetrics();
         Maps.entries(metrics).fold(metricRegistry, registerMetric());
     }
 
